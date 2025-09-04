0b4de0c03f692f270bf95567a1e1806e5500b3c1
Murilo 26 <00001114982313sp@al.educacao.sp.gov.br>
2025-09-04 19:44:10 -0300

    pinto

--- pandas.py
+++ pandas.py
@@ -1,4 +1,16 @@
 # TRABAIO DO TOMPSO
+import openpyxl
+import pandas as pd
+import os
+import pathlib
+
+escola = { "Aluno"      : [],
+           "Diciplina"   : [],
+           "notas"      : [] }
+
+print("""Olá, gostaria de fazer oq agora?
+      1°Mexer na planilha""")
+
 def fodase():
     return 0
 
