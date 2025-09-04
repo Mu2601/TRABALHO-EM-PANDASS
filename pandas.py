0ebeeb8a47e4d5d8f849a5096243cf45ca316fb3
Murilo 26 <00001114982313sp@al.educacao.sp.gov.br>
2025-09-04 20:09:05 -0300

    aaa

--- pandas.py
+++ pandas.py
@@ -4,34 +4,24 @@ import pandas as pd
 import os
 import pathlib
 
-<<<<<<< HEAD
 escola = { "Aluno"      : [],
            "Disciplina"   : [],
            "notas"      : [] }
-=======
 # Funcao base.
 def BancoDeDadosEscola( ):
     escola = { "Aluno"      : [],
                "Disciplina"   : [],
                "Notas"      : [] }
->>>>>>> 8eea30491efdcc78d886eea6640f30c009b4974f
 
-<<<<<<< HEAD
-print("""Olá, gostaria de fazer oq agora?
-      |1°Mexer na planilha |2° Abrir a planilha|""")
-escolha=int(input(" "))
-=======
     print("""Olá, gostaria de fazer oq agora?
-          |1°Mexer na planilha | 2° Abrir a planilha|""")
->>>>>>> 8eea30491efdcc78d886eea6640f30c009b4974f
+        |1°Mexer na planilha |2° Abrir a planilha|""")
+    escolha=int(input(" "))
 
-<<<<<<< HEAD
-if escolha == 1:
-    a=input("digite o nome do aluno: ")
-    escola["Aluno"]=a
-    print(escola)
-def fodase():
-    return 0
-=======
+    if escolha == 1:
+        a=input("digite o nome do Aluno: ")
+        escola["Aluno"]=a
+        a=input("digite o nome do Disciplina: ")
+        escola["Disciplina"]=a
+        print(escola)
+        
 BancoDeDadosEscola()
->>>>>>> 8eea30491efdcc78d886eea6640f30c009b4974f
